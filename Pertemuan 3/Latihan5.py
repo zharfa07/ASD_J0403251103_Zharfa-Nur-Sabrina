594b7964391359a93e5846c4d94cea1b656e4e56
zharfa07 <zharfans107@gmail.com>
2026-02-11 15:51:38 +0700

    Add files via upload

---
+++ Pertemuan 3/Latihan5.py
@@ -0,0 +1,47 @@
+#==============================================================================================================
+#Praktikum 2: Linked List
+#Latihan 5 : Tambahkan metode untuk membalik (reverse) sebuah single linked list tanpa membuat linked list baru
+#==============================================================================================================
+class node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class linkedlist:
+    def __init__(self):
+        self.head = None
+        self.tail = None 
+    
+    def insert_at_end(self, data):
+        new_node = node(data)
+        if not self.head:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail  = new_node
+
+    def display(self):
+            temp = self.head
+            while temp:
+                print(temp.data, end="->")
+                temp = temp.next    
+            print("null")
+
+    def reverve(self):
+        print("Hasil reverse : ")
+        temp = self.tail
+        while temp:
+            print(temp.data, end="->")
+            temp = temp.prev
+        print("null")
+
+r = linkedlist()
+r.insert_at_end(1)
+r.insert_at_end(2)
+r.insert_at_end(3)
+r.insert_at_end(4)
+r.insert_at_end(5)
+print(f'Linked list sebelum dibalik : {r.display()}')
+r.reverve()
+print(f'Linked list setelah dibalik : {r.reverve()}')
