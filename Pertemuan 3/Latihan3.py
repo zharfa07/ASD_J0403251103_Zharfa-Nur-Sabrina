bc208d5a8229f4df14c9521e59cd88791720d4d2
zharfa07 <zharfans107@gmail.com>
2026-02-11 17:09:59 +0700

    Update Latihan3.py

--- Pertemuan 3/Latihan3.py
+++ Pertemuan 3/Latihan3.py
@@ -5,41 +5,44 @@
 
 class node:
     def __init__(self, data):
-        self.data = data
-        self.next = None
-        self.prev = None
+        self.data = data #menyimpan nilai data pada node
+        self.next = None #pointer ke node berikutnya
+        self.prev = None #pointer ke node sebelumnya
 
 class doublylinkedlist:
     def __init__(self):
-        self.head = None
-        self.tail = None
+        self.head = None #node pertama
+        self.tail = None # node terakhir 
     
     def insert_at_end(self, data):
-        new_node = node(data)
-        if not self.head:
-            self.head = new_node
+        new_node = node(data) #membuat node baru
+        if not self.head:         #jika list masih kosong
+            self.head = new_node 
             self.tail = new_node
         else:
-            self.tail.next = new_node
+            self.tail.next = new_node #sambung node terakhir ke node baru
             new_node.prev = self.tail
-            self.tail = new_node
+            self.tail = new_node #update
     
     def search(self, key):
         temp = self.head
         while temp:
-            if temp.data == key:
+            if temp.data == key: #jika ditemukan
                 return True
-            temp = temp.next
-        return False
+            temp = temp.next #ke node berikutnya 
+        return False #jika tidak ditemukan
 
 dll = doublylinkedlist()
+
+#menambahkan data
 dll.insert_at_end(2)
 dll.insert_at_end(6)
 dll.insert_at_end(9)
 dll.insert_at_end(14)
 dll.insert_at_end(20)
-key = 0 
+key = 0 #nilai yang dicari
 if dll.search(key):
     print(f"elemen {key} ditemukan")
 else:
+
     print(f"elemen {key} tidak ditemukan")
